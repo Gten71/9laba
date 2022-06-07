a4ad24d3a72aa3801bda48a12b4307a562a81327
Gten <mresnyansky@gmail.com>
2022-06-07 20:39:08 +0200

    Добавьте файлы проекта.

---
+++ 9laba/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] a = Enumerable.Range(0, 11)
+    .Select(i =>
+    {
+        Console.Write($"Введите a[{i}] = ");
+        return int.Parse(Console.ReadLine());
+    }).ToArray();
+int sum = a.Where(x => x % 2 == 1).Sum();
+Console.WriteLine($"Сумма нечетных чисел = {sum}");
