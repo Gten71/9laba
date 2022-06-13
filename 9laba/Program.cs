6eb9810380271abb71e959f8754cac74ccc790d9
Maxim <83307844+Gten71@users.noreply.github.com>
2022-06-13 11:42:22 +0200

    Update Program.cs

--- 9laba/Program.cs
+++ 9laba/Program.cs
@@ -1,8 +1,18 @@
-﻿int[] a = Enumerable.Range(0, 11)
+int[] a = Enumerable.Range(0, 11)
     .Select(i =>
     {
         Console.Write($"Введите a[{i}] = ");
         return int.Parse(Console.ReadLine());
     }).ToArray();
+
 int sum = a.Where(x => x % 2 == 1).Sum();
-Console.WriteLine($"Сумма нечетных чисел = {sum}");
+foreach (int x in a)
+{
+    int b = x % 2;
+    if (b != 0)
+    {
+        sum += x;
+
+    }
+}
+    Console.WriteLine($"Сумма нечетных (отрицательных) чисел = {sum}");
